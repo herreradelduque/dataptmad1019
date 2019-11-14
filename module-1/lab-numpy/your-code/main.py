ca5df6b0919cc7b62e71773cafaa73def2f41511
Víctor Galán <herreradelduque@gmail.com>
2019-11-15 00:18:09 +0100

    partially done

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,45 +1,60 @@
 #1. Import the NUMPY package under the name np.
 
-#inital commit
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2,3,5))
 
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+print(f'Size of a: {np.shape(a)}')
+print(f'Size of b: {np.shape(b)}')
 
+if np.shape(a) != np.shape(b):
+    print('a and b have different shape')
 
+else:
+    print('a and b have the same shape')
 
 #8. Are you able to add a and b? Why or why not?
 
+#a + b
+#unable to add a and b, because Numpy raise: "TypeError: only integer scalar arrays can be converted to a scalar index"
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = b.transpose()
+
+print(c)
+
+print(c.shape)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
